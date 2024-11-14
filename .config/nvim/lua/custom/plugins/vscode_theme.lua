795f6e16fe35c1e789d34c3dce2255124a605de2
SBlokhuizen <seb_vb@hotmail.com>
2024-11-14 23:43:50 +0100

    move to modular kickstart

---
+++ .config/nvim/lua/custom/plugins/vscode_theme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'askfiy/visual_studio_code',
+    priority = 1000,
+    config = function()
+      vim.cmd [[colorscheme visual_studio_code]]
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
