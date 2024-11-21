40d81f91ca82d33f73b9d1daa199f12dc10879ef
Sebbe <sblokhuizen@ignitioncomputing.com>
2024-11-21 23:24:13 +0100

    change diff colors

--- .config/nvim/lua/plugins/vscode_theme.lua
+++ .config/nvim/lua/plugins/vscode_theme.lua
@@ -4,6 +4,12 @@ return {
     priority = 1000,
     config = function()
       vim.cmd [[colorscheme vscode]]
+
+      -- Override diff colors
+      -- vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#26332c', fg = nil })
+      vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#1c3448', fg = nil })
+      -- vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#3f2d2d', fg = nil })
+      vim.api.nvim_set_hl(0, 'DiffText', { bg = '#365d78', fg = nil })
     end,
   },
 }
