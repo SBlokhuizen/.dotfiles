946a894f5d912fd673c805e19043f8ca9eef6da9
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-04-18 10:11:35 +0200

    remove multicursor keybindings due to bug

--- .config/nvim/lua/plugins/multicursor.lua
+++ .config/nvim/lua/plugins/multicursor.lua
@@ -2,9 +2,5 @@ return {
   {
     'mg979/vim-visual-multi',
     branch = 'master',
-    config = function()
-      vim.keymap.set('n', '<C-k>', '<Plug>(VM-Add-Cursor-Up)')
-      vim.keymap.set('n', '<C-j>', '<Plug>(VM-Add-Cursor-Down)')
-    end,
   },
 }
