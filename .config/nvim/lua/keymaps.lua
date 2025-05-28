d62c12694ee1dcac952ec737d4a61e5e183c348b
Sebbe <sblokhuizen@ignitioncomputing.com>
2025-05-28 14:22:02 +0200

    update gl and nvim window keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -31,6 +31,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Rebind split  keys
+vim.keymap.set('n', '<C-w>-', ':split<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-w>/', ':vsplit<CR>', { noremap = true, silent = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
