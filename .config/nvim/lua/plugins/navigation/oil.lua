4beef6ac6c63c3cc92aa78a2a7617b2638ab1cfc
Sebbe Blokhuizen <sblokhuizen@ignitioncomputing.com>
2026-04-02 16:49:05 +0200

    fix oil issue

--- .config/nvim/lua/plugins/navigation/oil.lua
+++ .config/nvim/lua/plugins/navigation/oil.lua
@@ -28,14 +28,21 @@ return {
         },
       }
 
-      -- auto-preview when cursor moves (only in oil buffers)
+      -- auto preview on cursor move (oil buffers only)
       vim.api.nvim_create_autocmd('FileType', {
         pattern = 'oil',
         callback = function()
           vim.api.nvim_create_autocmd('CursorMoved', {
             buffer = 0,
             callback = function()
-              require('oil.actions').preview.callback()
+              local ok, entry = pcall(require('oil').get_cursor_entry)
+              if not ok or not entry then
+                return
+              end
+
+              pcall(function()
+                require('oil.actions').preview.callback()
+              end)
             end,
           })
         end,
